b1d1599bacbbf7bb27b2a22fc7b5e93fcf41ad87
Hlaluko <ngobenih45@gmail.com>
2026-08-05 23:31:04 +0200

    Implement scroll progress bar functionality
    Add a scroll event listener to update the progress bar.

--- script.js
+++ script.js
@@ -227,3 +227,16 @@ if (form) {
         form.reset();
     });
 }
+/* Progress Bar */
+
+window.addEventListener("scroll",()=>{
+
+const scrollTop=document.documentElement.scrollTop;
+
+const scrollHeight=document.documentElement.scrollHeight-document.documentElement.clientHeight;
+
+const progress=(scrollTop/scrollHeight)*100;
+
+document.getElementById("progress-bar").style.width=progress+"%";
+
+});
